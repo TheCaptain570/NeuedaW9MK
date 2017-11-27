2919526f1eb3f27178b149a722d39d4023a78a5a
TheCaptain570 <matthew.king.bmc@gmail.com>
2017-11-27 13:56:32 +0000

    Inital Commit

---
+++ Stuff2.0/src/MoreStuff/Log.java
@@ -0,0 +1,49 @@
+package MoreStuff;
+
+public class Log {
+	
+	public static void main(String[] args){
+		log("dihjfgbewrauiyghbvrieouhb");
+		}
+	
+
+
+	
+    @SuppressWarnings("unused")
+	private static void logln(String number)
+    {
+        System.out.println(number);
+    
+    }
+    @SuppressWarnings("unused")
+	private static void logln(double number)
+    {
+        System.out.println(number);
+    
+    }
+    @SuppressWarnings("unused")
+	private static void logln(int number)
+    {
+        System.out.println(number);
+    
+    }
+    private static void log(String number)
+    {
+        System.out.print(number);
+    
+    }
+    @SuppressWarnings("unused")
+	private static void log(double number)
+    {
+        System.out.print(number);
+    
+    }
+    @SuppressWarnings("unused")
+	private static void log(int number)
+    {
+        System.out.print(number);
+    
+    }
+
+
+}
