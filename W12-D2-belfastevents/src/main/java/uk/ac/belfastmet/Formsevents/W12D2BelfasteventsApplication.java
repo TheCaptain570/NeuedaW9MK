5dc8ae3236f5ae024fe48426737db7a743399612
TheCaptain570 <matthew.king.bmc@gmail.com>
2017-12-19 11:53:22 +0000

    W12 D2 belfastevents pt2

---
+++ W12-D2-belfastevents/src/main/java/uk/ac/belfastmet/Formsevents/W12D2BelfasteventsApplication.java
@@ -0,0 +1,12 @@
+package uk.ac.belfastmet.Formsevents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W12D2BelfasteventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(W12D2BelfasteventsApplication.class, args);
+	}
+}
---
+++ W12-D2-belfastevents/src/test/java/uk/ac/belfastmet/Formsevents/W12D2BelfasteventsApplicationTests.java
@@ -0,0 +1,16 @@
+package uk.ac.belfastmet.Formsevents;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class W12D2BelfasteventsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
